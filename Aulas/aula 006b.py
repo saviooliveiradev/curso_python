e116d50643cd1fee2537c5ce26686f1c38abc71e
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-19 21:25:48 -0300

    aula 006 B

---
+++ Aulas/aula 006b.py
@@ -0,0 +1,8 @@
+n = str(input('Digite um valor: '))
+
+print(n.isnumeric()) # ele vai dizer se é possivel converter esse valor em número 
+print(n.isalpha()) # vai infomar se é letra
+print(n.isalnum()) # vai infomar número e letra na mesma palavra 
+print(n.isupper()) # vai infomar letra maiúscula 
+print(n.islower()) # vai infomar letra minúscula
+print(n.isspace()) # vai infomar se é uma espaço 
