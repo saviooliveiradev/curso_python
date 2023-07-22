a03e4eb46f7c2a426f749006c61629a432b8a0e4
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-22 12:49:44 -0300

    Desafio 11
    Aula 7

---
+++ Desafios/ds011.py
@@ -0,0 +1,12 @@
+# aula 7
+
+largura = float(input('Digite a largura da sua parede em metros: '))
+altura = float(input('Digite a altura da sua parede em metros: '))
+
+area = largura * altura
+
+tinta = (area / 2) 
+
+print(f'A sua área é {area}, para pintar essa área vamos precisar de {tinta}L de tinta')
+
+
