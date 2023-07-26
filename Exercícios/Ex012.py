7d3c315e36da30c9687ddd5e423b1d160ef5a398
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 11:52:08 -0300

    Ex012
    aula 7

---
+++ Exercícios/Ex012.py
@@ -0,0 +1,8 @@
+# aula 7
+
+preço = float(input('Digie o preço de um produto:R$ '))
+
+desconto = preço - (preço * 5 / 100)
+
+
+print(f'O preço do produto com desconto de 5%  fica de R$ {desconto:.2f}')
