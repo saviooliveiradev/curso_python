09db9fe73ff6ece2f4b0127d53e7fdef3af94bef
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 19:13:04 -0300

    aula 008b

---
+++ Aulas/aula 008b.py
@@ -0,0 +1,8 @@
+import emoji
+import random #uma biblioteca com a funcionalidade para gerar números aleatórios enntre 0 e 1, um número float.
+
+num = random.randint(1, 10) # vai gerar um número entre 1 e 10 aleatóriamente.
+print(num)
+
+print(emoji.emojize("Olá, mundo :smiling_face_with_sunglasses: :kitchen_knife:"))
+print(emoji.emojize(":kitchen_knife:"))
