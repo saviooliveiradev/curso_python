ada3684d93536c3a1bcbc1caa462404df034029c
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 11:53:02 -0300

    atualização

--- Desafios/ds013.py
+++ Desafios/ds013.py
@@ -2,6 +2,6 @@
 
 salario = float(input('Digite o seu salário:R$ '))
 
-novo_salario = (salario * 15 / 100) + salario
+novo_salario = salario + (salario * 15 / 100) 
 
 print(f'Seu novo salário é R${novo_salario}')
