46a25d24f34e77f33c6fa3d2d688cf575558ea00
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 19:15:11 -0300

    ds015
    aula 7

---
+++ Desafios/ds015.py
@@ -0,0 +1,8 @@
+# aula 7
+
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos km rodados? '))
+
+aluguel = (60 * dias) + (km * 0.15)
+
+print(f'O total a pagar é de R${aluguel}')
