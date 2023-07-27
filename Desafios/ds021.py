6e0929240d9293ed64b59a2356ecbc9c13fb72e3
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 22:09:04 -0300

    ds021
    completo

--- Desafios/ds021.py
+++ Desafios/ds021.py
@@ -1,1 +1,16 @@
 # aula 8
+
+import pygame #biblioteca utilizada para jogos
+
+# primeira regra da bib, termos que iniciar ele
+pygame.mixer.init()
+pygame.init()
+
+# nome do arquivo
+pygame.mixer.music.load('Desafios\musica.mp3')
+
+# play na música
+pygame.mixer.music.play()
+
+# encerrar quando parar de tocar a música
+pygame.event.wait()
