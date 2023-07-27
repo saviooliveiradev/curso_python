710c170bd1ecea2d6d189c95383d56e70b26d822
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-26 21:39:26 -0300

    Ex016

---
+++ Exercícios/Ex016.py
@@ -0,0 +1,10 @@
+# aula 8
+
+'''from math import trunc
+
+num1 = float(input('Digite um número: '))
+
+print(f'O número {num1} tem a parte inteira de {trunc(num1)}')'''
+
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')
