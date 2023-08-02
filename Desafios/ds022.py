fb74a6c751141c166e9fa00f6849303540bb476a
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-02 18:58:23 -0300

    ds020
    Aula 9

---
+++ Desafios/ds022.py
@@ -0,0 +1,10 @@
+# Aula 9
+
+nome = str(input('Digite seu nome completo: '))
+
+print(nome.upper())
+print(nome.lower())
+dividido = nome.split()
+junção = ''.join(dividido)
+print(len(junção))
+print(len(dividido[0]))
