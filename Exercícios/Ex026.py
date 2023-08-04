d733f3ad4e4288cd077a30784e885094b8dabcee
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-03 22:02:46 -0300

    Ex026
    aula 9

---
+++ Exercícios/Ex026.py
@@ -0,0 +1,7 @@
+# aula 9
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+
+print(f'A letrra A aparece {frase.count("A")} vezes na frase')
+print(f'A primeira letra A apareceu na posição {frase.find("A")+1}')
+print(f'A ultima letra A apareceu {frase.rfind("A")+1}')
