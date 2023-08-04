4264033d4be3349af96ce36cd27fbbdc79ac0697
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-03 22:27:52 -0300

    Ex027
    aula 9

---
+++ Exercícios/Ex027.py
@@ -0,0 +1,8 @@
+# aula 9
+
+n = input('Digite seu nome completo: ').strip()
+nome = n.split()
+
+print('Muito prazer em te conhecer')
+print(f'Seu primeiro nome é {nome[0]}')
+print(f'Seu último nome é {nome[len(nome)-1]}')
