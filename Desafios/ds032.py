309b9f98f5522278dc44659ed7c0aa188d7ec5d1
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-15 20:01:36 -0300

    ds032
    completo

--- Desafios/ds032.py
+++ Desafios/ds032.py
@@ -1,9 +1,13 @@
 # aula 10
 
-ano = int(input('Digite um ano: '))
+from datetime import date
 
-calculo = 0
-if calculo == 0:
-    print('Não é ano bissexto')
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é BISSEXTO')
 else:
+    print(f'O ano {ano} NÃO é BISSEXTO')
     print('É um ano bissexto')
