b462b80893b86ab9154a7b91dbc0662ce25a1ddc
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-23 21:38:25 -0300

    ds043
    aula 12

---
+++ MUNDO 2/Desafios/ds043.py
@@ -0,0 +1,17 @@
+# aula 12
+
+peso = float(input('Digite seu peso em Kg: '))
+altura = float(input('Digite sua altura: '))
+
+imc = peso / altura**2
+
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc >=  18.5 and imc == 25:
+    print('Peso ideal')
+elif imc >= 25 and imc == 30:
+    print('Sobrepeso')
+elif imc >30 or imc == 40:
+    print('Obesidade')
+elif imc > 40:
+     print('Obesidade mórbida')
