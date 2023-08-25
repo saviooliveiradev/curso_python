ac63a9c75dc66f7366deaf9d922d65a080af6a9f
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-25 20:28:01 -0300

    Ex039 2.0
    atribuindo mais condiçõess

--- MUNDO 2/Exercícios/Ex039.py
+++ MUNDO 2/Exercícios/Ex039.py
@@ -4,20 +4,24 @@ from datetime import date #usando módulos para pegar o ano atual
 
 atual = date.today().year #usando o método (date.today()), e atribuição de (year)  
 nascimento = int(input('Ano de nascimento: '))
- 
-idade = atual - nascimento
+sexo = str(input('Qual é o seu sexo:'))
 
-print(f'Quem nasceu em {nascimento} tem {idade} anos em {atual}')
+if sexo == 'feminino' or sexo == 'mulher':
+    print('Você não faz alistamento!')
+elif sexo == 'maisculino' or sexo == 'homem':
+    idade = atual - nascimento
 
-if idade == 18:
-    print('Você tem que se alistar IMEDIATAMENTE!!')
-elif idade < 18:
-    saldo = 18 - idade
-    print(f'Você ainda não tem 18 anos. Ainda faltam {saldo} anos para o alistamento')
-    ano = atual + saldo
-    print(f'Seu alistamento será em {ano}')
-elif idade > 18:
-    saldo = idade - 18
-    print(f'Você já deveria ter se alistado há {saldo}')
-    ano =  atual - saldo
-    print(f'Seu alistamento foi em {ano}')
+    print(f'Quem nasceu em {nascimento} tem {idade} anos em {atual}')
+
+    if idade == 18:
+        print('Você tem que se alistar IMEDIATAMENTE!!')
+    elif idade < 18:
+        saldo = 18 - idade
+        print(f'Você ainda não tem 18 anos. Ainda faltam {saldo} anos para o alistamento')
+        ano = atual + saldo
+        print(f'Seu alistamento será em {ano}')
+    elif idade > 18:
+        saldo = idade - 18
+        print(f'Você já deveria ter se alistado há {saldo}')
+        ano =  atual - saldo
+        print(f'Seu alistamento foi em {ano}')
