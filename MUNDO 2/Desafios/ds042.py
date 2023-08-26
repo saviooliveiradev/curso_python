7b89836254d34fe979a0be08357c4b1ac65b9b35
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-08-25 21:09:09 -0300

    ds042 2.0
    completo !

--- MUNDO 2/Desafios/ds042.py
+++ MUNDO 2/Desafios/ds042.py
@@ -8,9 +8,14 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceito segmento: '))
 
-if r1 == r2 or r1 == r3 or r2 == r1 or r2 == r3 or r3 == r1 or r3 == r2:
-    print('\033[34mEQUILÁTERO\033[m')
-elif r1 == r2:
-    print('\033[34mISÓSCELES\033[m')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo', end='') # não vai ter fim da linha
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓCELES')
+           
 else:
-    print('\033[34mESCALENO\033[m')
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
