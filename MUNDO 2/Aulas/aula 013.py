a3d8d3b0f4c9641e56e43cbf3500621f5e287443
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-10-02 16:51:48 -0300

    Aula 013

---
+++ MUNDO 2/Aulas/aula 013.py
@@ -0,0 +1,15 @@
+#colocando 1 na frente ele não considera o último número
+for c in range(1, 6):  
+    print('oi')
+print('fim')
+
+# contando para trás
+for c in range(6, 0, -1):  
+    print(c)
+print('fim')
+
+# pulando em 2 em 2
+for c in range(0, 7, 2):  
+    print(c)
+print('fim')
+
