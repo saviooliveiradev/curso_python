496a067ffa7aeb69e5bf93f2189c78c9b5036265
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-11-27 19:19:39 -0300

    Ex051
    Progressão Aritmética

---
+++ MUNDO 2/Exercícios/Ex051.py
@@ -0,0 +1,8 @@
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10-1) * razão
+
+for c in range (primeiro, décimo + razão, razão):
+    print(f'{c}', end="=")
+print('Acabou')
+      
