843bafa73f30b4e627c2523ed8f7ee2b9abc1189
saviooliveira_dev <saviooliveiratec@gmail.com>
2024-01-17 20:18:33 -0300

    Ex053 - segunda forma

--- MUNDO 2/Exercícios/Ex053.py
+++ MUNDO 2/Exercícios/Ex053.py
@@ -2,13 +2,9 @@ frase = str(input('Digite uma frase: ')).strip().upper() #deixa maiúsculo
 
 palavras = frase.split() # separando as palavras com o espaço na frase
 junto = ''.join(palavras) #vai juntar as palavrass com espaço
-inverso = ''
+inverso =  junto[::-1]
 
 print(f'o iverso de {junto} é {inverso}')
-
-for letra in range(len(junto) -1, -1, -1):
-    print(junto[letra])
-    inverso += junto[letra]
 
 if inverso == junto:
     print('temo um palíndromo')
